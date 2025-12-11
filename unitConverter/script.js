979c98b8608700e69e2dcdb86c7a7bc88ead5884
zynfx1 <sebigan.zyn@gmail.com>
2025-12-11 11:53:38 +0800

    orig msg

--- unitConverter/script.js
+++ unitConverter/script.js
@@ -6,6 +6,11 @@ const convertionRates = {
     millimeter: '0.001',
 };
 
+document.getElementById("form").addEventListener("submit", (e)=> {
+
+   e.preventDefault();
+});
+
 const inputAmount = document.getElementById("inputAmount");
 const fromSelect = document.getElementById("fromSelect");
 const toSelect = document.getElementById("toSelect");
@@ -17,7 +22,7 @@ const units = Object.keys(convertionRates);
 
 function optionSelect(){
     units.forEach(unit =>{
-        const label = unit.charAt(0).toUpperCase() + unit.slice(1);
+        const label = unit.charAt(0).toUpperCase() + unit.slice(1) ; 
 
         const optionFrom = new Option(label, unit);
         const optionTo = new Option(label, unit);
@@ -33,21 +38,18 @@ function optionSelect(){
 }
 
 //logic
-function calculation(){
+function calculation() {
     const amount = parseFloat(inputAmount.value);
     const fromUnit = fromSelect.value;
     const toUnit = toSelect.value;
 
-    const rateFrom = convertionRates[fromUnit];
-    const rateTo = convertionRates[toUnit];
+    const totalFrom = convertionRates[fromUnit];
+    const totalTo = convertionRates[toUnit];
 
-    const result = (amount * rateFrom) / rateTo;
+    const result = (amount * totalFrom) / totalTo;
 
-
-  outputResult.innerText = result;
-
+    outputResult.textContent = result.toLocaleString(undefined, {maximumFractionDigits: 4})+" "+ toUnit;
 }
-
 
 optionSelect();
 
