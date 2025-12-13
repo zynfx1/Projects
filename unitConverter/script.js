587a63e7bcea067af620b0d78aa5077cbcc007f8
zynfx1 <sebigan.zyn@gmail.com>
2025-12-13 21:22:14 +0800

    time conver

--- unitConverter/script.js
+++ unitConverter/script.js
@@ -1,15 +1,12 @@
+const timeRates = {
+    Seconds: '1',
+    Minutes: '60',
+    Hours: '3600',
+    Days: '86400',
 
-const convertionRates = {
-    meter: '1',
-    kilometer: '1000',
-    centimeter: '0.01',
-    millimeter: '0.001',
 };
 
-document.getElementById("form").addEventListener("submit", (e)=> {
-
-   e.preventDefault();
-});
+const time = Object.keys(timeRates);
 
 const inputAmount = document.getElementById("inputAmount");
 const fromSelect = document.getElementById("fromSelect");
@@ -17,42 +14,37 @@ const toSelect = document.getElementById("toSelect");
 const convertBtn = document.getElementById("convertBtn");
 const outputResult = document.getElementById("outputResult");
 
-const units = Object.keys(convertionRates);
-
-
 function optionSelect(){
-    units.forEach(unit =>{
-        const label = unit.charAt(0).toUpperCase() + unit.slice(1) ; 
+    time.forEach(units => {
+        const label = units.charAt(0).toUpperCase() + units.slice(1);
 
-        const optionFrom = new Option(label, unit);
-        const optionTo = new Option(label, unit);
-
+        const optionFrom = new Option (label, units);
+        const optionTo = new Option (label, units);
 
         fromSelect.add(optionFrom);
         toSelect.add(optionTo);
+
     });
+    fromSelect.value = "Seconds";
+    toSelect.value = "Minutes";
 
-    fromSelect.value = 'meter';
-    toSelect.value = 'kilometer';
+};
 
-}
 
-//logic
-function calculation() {
-    const amount = parseFloat(inputAmount.value);
-    const fromUnit = fromSelect.value;
-    const toUnit = toSelect.value;
+function conversionTime (){
+const amount = parseFloat(inputAmount.value);
+const fromTime = fromSelect.value;
+const toTime = toSelect.value;
 
-    const totalFrom = convertionRates[fromUnit];
-    const totalTo = convertionRates[toUnit];
+const timeRateFrom = timeRates[fromTime];
+const timeRateTo = timeRates[toTime];
 
-    const result = (amount * totalFrom) / totalTo;
+const result = (amount * timeRateFrom) / timeRateTo;
 
-    outputResult.textContent = result.toLocaleString(undefined, {maximumFractionDigits: 4})+" "+ toUnit;
-}
+outputResult.textContent = result.toLocaleString(undefined, {maximumFractionDigits: 4})+ " "+toTime;
+
+};
 
 optionSelect();
 
-
-
-convertBtn.addEventListener("click", calculation);
+convertBtn.addEventListener("click", conversionTime);
