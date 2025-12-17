3ccdc8511da0681751fc67ed4305dc7f06074dc0
zynfx1 <sebigan.zyn@gmail.com>
2025-12-17 23:04:37 +0800

    vue level 1

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div class="bg-jungle-green-900 max-w-screen max-h-screen text-white">
+  <header class="bg-jungle-green-950 w-full h-20 flex items-center justify-between px-10">
+      <img src="./logo5.png" alt="Logo" class="h-full py-1"/>
+    <nav>
+      <ul>
+        <li class="flex gap-10">
+          <a href="#">HOME</a>
+          <a href="#">ABOUT</a>
+          <a href="#">SERVICES</a>
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 
-<style scoped></style>
+    <section class="bg-jungle-green-800 w-full h-screen flex items-center justify-center font-poppins text-2xl">
+      Section Content 1
+    </section>
+
+     <section class="bg-jungle-green-700 w-full h-screen flex items-center justify-center font-poppins text-2xl">
+      Section Content 2
+    </section>
+  
+  <footer class="w-full h-20 bg-jungle-green-950 flex items-center justify-center font-poppins text-lg">
+    My Vue App Footer
+  </footer>
+  </div>
+</template>
