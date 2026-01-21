0e5b61c8fc4071d3a2ac5cc3397ecae32f70cd0c
zynfx1 <sebigan.zyn@gmail.com>
2026-01-21 23:22:33 +0800

    feat(Frontend): Add initial components and styles for the Vue.js todo application

--- todo-app/Frontend/vue-project/src/App.vue
+++ todo-app/Frontend/vue-project/src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routerKey, RouterView } from 'vue-router';
+import router from './router';
+import Homepage from './views/HomepageView.vue';
+import Header from './components/headerComp.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="">
+    <Header />
+    <Homepage />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ todo-app/Frontend/vue-project/src/views/HomepageView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import router from '@/router/index';
+</script>
+
+<template>
+  <div class="w-max-full h-screen bg-pine-teal-700">aad</div>
+</template>
