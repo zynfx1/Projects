61ddfca3c120790f8e253bf8b7536c25e3212549
zynfx1 <sebigan.zyn@gmail.com>
2026-02-22 23:41:37 +0800

    feat: implement about and contact modal components with state management

--- todo-app/Frontend/vue-project/src/views/TodoView.vue
+++ todo-app/Frontend/vue-project/src/views/TodoView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { nextTick, onMounted, ref, watch } from 'vue';
+import { useAboutModalStore, useContactModalStore, activeTab } from '@/stores/headerModalStore';
 import { useCreateModalStore, createItem, completedStore } from '@/stores/CreateItemStore';
 import todosCard from '@/components/todosCard.vue';
 import type { todoUser } from '@/types/todoUser';
+const contactModalStore = useContactModalStore();
+const aboutModalStore = useAboutModalStore();
 const modalStore = useCreateModalStore();
 const todos = createItem();
 const todosComplete = completedStore();
-const activeTab = ref('todo-list');
 
 onMounted(async () => {
   //todos.selectNotCompleteTodos();
@@ -20,16 +22,22 @@ onMounted(async () => {
       <ul>
         <li class="flex items-center justify-center gap-0.5 text-center">
           <a
+            @click="
+              (aboutModalStore.closeAboutModal('todo-list'),
+              contactModalStore.closeContactModal('todo-list'))
+            "
             :class="{ 'bg-jungle-green-900 text-white': activeTab === 'todo-list' }"
             class="bg-custom-green hover:bg-jungle-green-900 relative h-full cursor-pointer rounded-sm px-15 py-3 transition duration-300 ease-in-out hover:gap-0 hover:text-white"
             >Todo List</a
           >
           <a
+            @click="aboutModalStore.openAboutModal('about')"
             :class="{ 'bg-jungle-green-900 text-white': activeTab === 'about' }"
             class="hover:bg-jungle-green-900 bg-custom-green h-full cursor-pointer rounded-sm px-15 py-3 transition duration-300 ease-in-out hover:text-white"
             >About</a
           >
           <a
+            @click="contactModalStore.openContactModal('contact')"
             :class="{ 'bg-jungle-green-900 text-white': activeTab === 'contact' }"
             class="hover:bg-jungle-green-900 bg-custom-green h-full rounded-sm px-15 py-3 transition duration-300 ease-in-out hover:text-white"
             >Contact</a
@@ -41,7 +49,7 @@ onMounted(async () => {
       loading...
     </div>
     <div
-      class="text-jungle-green-900 flex flex-col rounded-2xl border border-gray-500/10 bg-white p-8 shadow-2xl shadow-gray-400/50 lg:h-3/4 lg:w-3/4 2xl:h-3/4 2xl:w-3/4"
+      class="text-jungle-green-900 flex flex-col rounded-2xl border border-gray-500/10 bg-white p-8 shadow-2xl shadow-gray-400/50 lg:min-h-50 lg:w-3/4 2xl:min-h-150 2xl:w-3/4"
     >
       <header class="my-2 text-3xl text-black">Tasks:</header>
       <div v-if="todos.notCompleteTodosList.length > 0" class="">
@@ -73,12 +81,12 @@ onMounted(async () => {
           </div>
         </div>
       </div>
-      <div class="my-2 flex h-full w-full items-center justify-end">
+      <div class="fixed flex items-center justify-end 2xl:top-210 2xl:right-10">
         <button @click="modalStore.openModal">
           <img
             src="../assets/img/plus(2).png"
             alt=""
-            class="bg-jungle-green-900 mx-4 h-17 w-17 cursor-pointer rounded-full border border-gray-500/50 p-5.5 transition duration-200 ease-in-out hover:scale-105"
+            class="bg-jungle-green-900 mx-4 h-20 w-20 cursor-pointer rounded-full border border-gray-500/50 p-6 transition duration-200 ease-in-out hover:scale-105"
           />
         </button>
       </div>
