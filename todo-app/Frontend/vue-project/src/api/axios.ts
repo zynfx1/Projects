fd794a6aa80d272f4dd7fcc7b540b7c67db775e1
zynfx1 <sebigan.zyn@gmail.com>
2026-02-23 20:41:56 +0800

    fix: cors and axios urls

--- todo-app/Frontend/vue-project/src/api/axios.ts
+++ todo-app/Frontend/vue-project/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.PROD
-    ? 'https://todo-app-eo09.onrender.com/'
+    ? 'https://todo-app-eo09.onrender.com'
     : 'http://localhost:3000/api',
   timeout: 5000,
 });
