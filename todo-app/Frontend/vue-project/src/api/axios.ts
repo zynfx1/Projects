ab67b3099938a00b1e9611bcb341f32089d44732
zynfx1 <sebigan.zyn@gmail.com>
2026-02-23 20:26:56 +0800

    feat: configure axios base URL for production and add Vercel configuration file

--- todo-app/Frontend/vue-project/src/api/axios.ts
+++ todo-app/Frontend/vue-project/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.PROD
+    ? 'https://todo-app-eo09.onrender.com/'
+    : 'http://localhost:3000/api',
   timeout: 5000,
 });
 
